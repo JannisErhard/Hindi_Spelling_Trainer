dbf2119c7247a310659c8c95139a9f7617f7eff4
JannisErhard <jannis.erhard@gmx.de>
2023-10-18 22:42:32 +0200

    prettified

--- Main.py
+++ Main.py
@@ -12,7 +12,6 @@ with open('example_vocabulary') as f:
 
 def select(value):
     global state, k, vocab, vocabulary 
-    print(k)
     length=len(l2.get())
     if (value=='Erase'):
         l2.delete(length-1,length)
@@ -22,16 +21,16 @@ def select(value):
     elif value=='Enter':
         if state:
             if l2.get() == vocab[1]:
-                l3=tk.Label(mWindow,text=' '*20,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
+                l3=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
                 l3=tk.Label(mWindow,text='Correct',bg='lightgrey',fg='green', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
             else :
-                l3=tk.Label(mWindow,text=' '*20,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
+                l3=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
                 l3=tk.Label(mWindow,text='Incorrect. Proper Solution: '+vocab[1],bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
             state = False
         else:
             k+=1
-            print(k), print(len(vocabulary))
             vocab = vocabulary[k]
+            l1=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='green', font=('Times', 24)).grid(row=0,column=0,columnspan=13)
             l1=tk.Label(mWindow,text=f"Translate \"{vocab[0]}\"",bg='lightgrey',fg='green', font=('Times', 24)).grid(row=0,column=0,columnspan=13)
             l3=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
             l3=tk.Label(mWindow,text='??????????',bg='lightgrey',fg='black', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
@@ -43,9 +42,11 @@ def select(value):
 state = True 
 k=0
 vocab = vocabulary[k]
+l1=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='green', font=('Times', 24)).grid(row=0,column=0,columnspan=13)
 l1=tk.Label(mWindow,text=f"Translate \"{vocab[0]}\"",bg='lightgrey',fg='green', font=('Times', 24)).grid(row=0,column=0,columnspan=13)
 l2=tk.Entry(mWindow,bg='black',fg='white',relief='raised', font=('Times', 24))
 l2.grid(row=1,column=0,columnspan=13)
+l3=tk.Label(mWindow,text=' '*80,bg='lightgrey',fg='red', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
 l3=tk.Label(mWindow,text='??????????',bg='lightgrey',fg='black', font=('Times', 24)).grid(row=2,column=0,columnspan=13)
 
 
