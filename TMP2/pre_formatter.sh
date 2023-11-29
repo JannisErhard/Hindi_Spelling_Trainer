7451745b5fbcf1ba6271cc4469abcbf60c1e483c
JannisErhard <jannis.erhard@gmx.de>
2023-11-29 10:56:45 +0100

    finished noew formatter

--- TMP2/pre_formatter.sh
+++ TMP2/pre_formatter.sh
@@ -1,1 +1,1 @@
-grep -Ei "hindi|label" raw | sed "s/\\\begin{hindi}//g"  | sed "s/\\\end{hindi}//g" | sed "s/\\\\//g"
+grep -Ei "hindi|label" raw | sed "s/\\\begin{hindi}//g"  | sed "s/\\\end{hindi}//g" | sed "s/\\\\//g" | tac
