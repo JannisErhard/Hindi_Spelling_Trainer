d302bc2d01b20eb3f0781e0f71ed89236a925907
JannisErhard <jannis.erhard@gmx.de>
2024-03-25 10:42:28 +0100

    bugfix

--- format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in *
-do  echo \"$i\"\:; nlines=`wc -l $i | awk '{print $1}'`
-       	awk -v n=$nlines 'NR==1{print "[[",$1,",", $2,"]"}NR>1&&NR<n{print ", [",$1,",", $2,"]"}NR==n{print ", [",$1,",", $2,"]]"}' $i
-done
